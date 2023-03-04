78174a63e382b4f5fca95e1d3726353cbd4f4758
Nelio Frazao <neliofrazac@gmail.com>
2023-03-03 21:17:15 -0300

    exemplos

--- src/App.js
+++ src/App.js
@@ -9,14 +9,53 @@
 // Prop drilling 
 // Componentes com estado
 
-import FirstComponent from './components/first-component'
+import { useState, useEffect } from 'react'
 import SubjectList from './components/subject-list'
 import StudentList from './Pages/student-list'
 
 const App = () => {
+  const [count, setCount] = useState(0)
+
+  const handleClick = () => setCount(count + 1)
+  const handleClickSubtrai = () => setCount(count - 1)
+
+  useEffect(() => { 
+    document.title = `Você clicou ${count} vezes.`    
+  }, [count])
+  
+
+  // const falaAgo = () => {
+    
+  //   const falaAluno = (fala1) => console.log(fala1);
+  //   const falaProfessor = (fala2) => console.log(fala2);
+
+  //   return [falaAluno, falaProfessor]
+
+  // }
+
+  // const [falaPessoa, falaProfessor] = falaAgo()
+
+  // falaPessoa('Pessoa Falou')
+  // falaProfessor('Aluno Falou')
+  
+
+  console.log(count)
+
+  const arrayComida = ['Lasanha', 'Hamburguer', 'Macarrâo']
+
+  const [Lasanha, Hamburguer, Macarrâo] = arrayComida
+
+  console.log(Lasanha)
+  console.log(Hamburguer)
+  console.log(Macarrâo)
+
+
   return (
     <div>
-      <h1>Alunos</h1>
+       <h1>contador: {count}</h1>
+      <button onClick={handleClick}>Soma</button> 
+      <button onClick={handleClickSubtrai}>Subtrai</button> 
+      
       <StudentList />
 
       {/* <FirstComponent /> */}
